e0e379237e1408e592c42b19574fed1842a5ab6e
anilyavas <anil.yavas99@gmail.com>
2025-06-28 14:27:39 +0300

    Update ExampleScan.tsx: add ActivityIndicator, modify styles and text

--- components/ExampleScan.tsx
+++ components/ExampleScan.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View, Text, Image, Pressable } from 'react-native';
+import { View, Text, Image, Pressable, ActivityIndicator } from 'react-native';
 
 export default function HowToScanOverlay() {
   const [visible, setVisible] = useState(true);
@@ -7,25 +7,26 @@ export default function HowToScanOverlay() {
   if (!visible) return null;
 
   return (
-    <View className="absolute inset-0 z-50 items-center justify-center bg-black/70">
+    <View className="m-6 items-center justify-center gap-2 rounded-lg bg-black/30 p-2">
       {/* Example image */}
       <Image
         source={{
           uri: 'https://nhgardensolutions.wordpress.com/wp-content/uploads/2024/03/18.-purple-crocuses.jpg?w=1024',
         }}
-        className="mb-6 h-80 w-80 rounded-lg"
+        className="m-6 h-60 w-60 rounded-lg"
         resizeMode="cover"
+        onLoad={() => <ActivityIndicator size={'large'} style={{ alignSelf: 'center' }} />}
       />
 
       {/* Instructions */}
-      <Text className="mb-4 px-6 text-center text-lg text-white">
+      <Text className="mb-4 px-6 text-center text-lg font-bold text-primary">
         Make sure the plant is centered and clear. Use good lighting and avoid cluttered
         backgrounds.
       </Text>
 
       {/* Button */}
       <Pressable onPress={() => setVisible(false)} className="rounded-full bg-green-700 px-6 py-3">
-        <Text className="p-2 text-lg font-bold text-white">I Understand</Text>
+        <Text className="p-2 text-lg font-bold text-white">I understand.</Text>
       </Pressable>
     </View>
   );
